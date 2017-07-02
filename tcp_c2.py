2955c8bd33879517107d53ca351370bde686dd9d
王亮亮 <1647855685@qq.com>
2017-07-02 21:46:33 +0800

    Delete tcp_c2.py

--- tcp_c2.py
+++
@@ -1,9 +0,0 @@
-import socket
-s2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s2.connect(('127.0.0.1',9998))
-print(s2.recv(1024))
-for data in ['wang','song','chen']:
-    s2.send(data.encode())
-    print(s2.recv(1024))
-
-#s2.send(b'exit')
