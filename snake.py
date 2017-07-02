1288d9bbffb190ac715bcdd9ed7c913bb9adb3e7
makalo <1647855685@qq.com>
2017-07-02 21:33:45 +0800

    TCP

--- snake.py
+++ snake.py
@@ -6,15 +6,15 @@ import tensorflow as tf
 import cv2
 import sys
 sys.path.append("game/")
-from tkinter import *
-
+#from tkinter import *
+import socket
 import win32api,win32con
-import t2
+import play
 import random
 import pygame
 import numpy as np
 import threading
-
+import time
 GAME = 'snake' # the name of the game being played for log files
 ACTIONS = 4 # number of valid actions
 EXPLORE = 2000000. # frames over which to anneal epsilon
@@ -22,6 +22,10 @@ FRAME_PER_ACTION = 1
 INITIAL_EPSILON = 0.05
 FINAL_EPSILON = 0.05
 
+mutex2 = threading.Lock()
+mutex = threading.Lock()
+port=8082
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev = 0.01)
     return tf.Variable(initial)
@@ -77,20 +81,28 @@ class createNetwork():
         self.train_step = tf.train.AdamOptimizer(1e-6).minimize(self.cost)
    
 def trainNetwork(current_q,sess):
+
+    
+    global sock1,addr1,sock2,addr2
+    global data
+    global data_2
+    data='10'
+    data_2='11'
+    t1=threading.Thread(target=tcp,args=(sock1, addr1))
+    t2=threading.Thread(target=tcp2,args=(sock2, addr2))
+    t1.start()
+    t2.start()
     pygame.mixer.init()
     pygame.mixer.Sound('audio/7301.wav').play()
-    game_state = t2.game(0,0,0)
     
-    #tkinter.messagebox.showinfo("规则","白色代表人工智能，蓝色代表人类，操作电脑上下左右键，z表示蓝蛇加速，和人工智能比赛吧")
-
-    #tkinter.messagebox.askokcancel('提示','这是一个消息框')
+    game_state = play.game(0,0,0)
     win32api.MessageBox(0,"白色代表人工智能(强化学习)\n蓝色代表人类\n操作电脑上下左右键，z表示蓝蛇加速\n按键1,2,3,4分别对应4种模式\n和人工智能比赛吧","规则",win32con.MB_OK)
     do_nothing = np.zeros(ACTIONS)
    
     do_nothing[random.randrange(ACTIONS)] = 1
     
-    s_t,terminal= game_state.frame_step(do_nothing)
-    s_t = cv2.cvtColor(cv2.resize(s_t, (80, 80)), cv2.COLOR_BGR2GRAY)
+    s_t,terminal,_= game_state.frame_step(do_nothing,data,data_2,mutex,mutex2)
+    s_t = cv2.cvtColor(cv2.resize(s_t.astype(np.uint8), (80, 80)), cv2.COLOR_BGR2GRAY)
     _, s_t = cv2.threshold(s_t,1,255,cv2.THRESH_BINARY)
     s_1 = np.stack((s_t, s_t, s_t, s_t), axis=2)
 
@@ -103,7 +115,10 @@ def trainNetwork(current_q,sess):
     else:
         print("Could not find old network weights")
     epsilon = INITIAL_EPSILON
+    feed_back=0
     while (True):
+        
+        
         # choose an action epsilon greedily
         readout_t = current_q.readout.eval(feed_dict={current_q.s : [s_1]})[0]
         a_t = np.zeros([ACTIONS])
@@ -124,13 +139,18 @@ def trainNetwork(current_q,sess):
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
         
         # run the selected action and observe next state and reward
-        s_t2,terminal= game_state.frame_step(a_t)
-        s_t2 = cv2.cvtColor(cv2.resize(s_t2, (80, 80)), cv2.COLOR_BGR2GRAY)
+        s_t2,terminal,feed_back= game_state.frame_step(a_t,data,data_2,mutex,mutex2)
+        
+        s_t2 = cv2.cvtColor(cv2.resize(s_t2.astype(np.uint8), (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, s_t2 = cv2.threshold(s_t2, 1, 255, cv2.THRESH_BINARY)
         s_t2 = np.reshape(s_t2, (80, 80, 1))
         s_2 = np.append(s_t2, s_1[:, :, :3], axis=2)
-
         s_1=s_2
+        if feed_back==1:
+        	sock2.send(b'10')
+        elif feed_back==2:
+        	sock2.send(b'11')
+        
 def playGame():
     
     current_q= createNetwork()
@@ -138,19 +158,45 @@ def playGame():
     trainNetwork(current_q,sess)
 def music():
     pygame.mixer.init()
+    time.sleep(5)
     while(True):
         pygame.mixer.Sound('audio/7895.wav').play()
-
-def main():
+        time.sleep(96)
+def tcp(sock,addr):
+    global data
     
-    # t1 = threading.Thread(target=music)
-    # t2 = threading.Thread(target=playGame)
-    # # t1.setDaemon(True)
-    # # t2.setDaemon(True)
+    while(True):
+        mutex.acquire()
+        data='10'
+        data,addr=sock.recvfrom(16)
+        
+        data=data.decode()
+def tcp2(sock,addr):
+    global data_2
+    
+    while(True):
+        mutex2.acquire()
+        data_2='11'
+        data_2,addr2=sock.recvfrom(16)
+        data_2=data_2.decode()
+        
+def main():
+    global sock1,addr1,sock2,addr2
+    s.bind(('127.0.0.1',9999))
+    s.listen(2)
+    sock1,addr1=s.accept()
+    sock2,addr2=s.accept()
+    # t1=threading.Thread(target=tcp,args=(sock1, addr1))
+    # t2=threading.Thread(target=tcp2,args=(sock2, addr2))
+    t4 = threading.Thread(target=playGame)
+    t3=threading.Thread(target=music)
+
     # t1.start()
     # t2.start()
+    t3.start()
+    t4.start()
 
-    playGame()
+    #playGame()
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,6 @@
+a='123456'
+print (a[0]+a[1])
+print (len(a))
+b='091'
+c=int(b)
+print (c)
